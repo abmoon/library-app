ab957941c12921f11a901bf56b4d260b892027c3
Avraham Luna <avraham.luna@gmail.com>
2016-01-21 02:35:18 -0800

    added JShint and JSlint files to the directory

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 
- var app = express();
+var app = express();
 
 var port = 5000;
 
@@ -8,16 +8,16 @@ app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 
-app.get('/', function(req, res){
-   res.send('Hello World!');
+app.get('/', function (req, res){
+    res.send('Hello World!');
 });
 
 
-app.get('/books', function(req, res){
-   res.send('Hello Books!');
+app.get('/books', function (req, res) {
+    res.send('Hello Books!');
 });
-      
-        
-app.listen(5000, function(err){
+
+
+app.listen(5000, function (err) {
     console.log('running server on port ' + port);
 });
